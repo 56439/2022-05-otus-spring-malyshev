b6c6863e8f4da9222c436f1201ada7a01b920f2f
Ivan Malyshev <i.malyshev@iac.spb.ru>
2022-06-17 22:39:08 +0300

    edit tests

--- hw-03-student-testing/src/test/java/ru/otus/hw03studenttesting/dao/QuestionDaoImplTest.java
+++ hw-03-student-testing/src/test/java/ru/otus/hw03studenttesting/dao/QuestionDaoImplTest.java
@@ -1,12 +1,16 @@
 package ru.otus.hw03studenttesting.dao;
 
 import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.otus.hw03studenttesting.config.AppConfiguration;
 import ru.otus.hw03studenttesting.model.Question;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -17,9 +21,16 @@ public class QuestionDaoImplTest {
     @Autowired
     QuestionDaoImpl questionDao;
 
+    @MockBean
+    AppConfiguration configuration;
+
     @Test
     @DisplayName(value = "Вопросы на русском")
     void getAllRu() {
+        Map<String, String> language = new HashMap<>();
+        language.put("ru", "questions_ru.csv");
+
+        Mockito.when(configuration.getQuestionsResources()).thenReturn(language);
         List<Question> questions = questionDao.getAll("ru");
 
         assertNotNull(questions);
@@ -28,6 +39,10 @@ public class QuestionDaoImplTest {
     @Test
     @DisplayName(value = "Вопросы на английском")
     void getAllEn() {
+        Map<String, String> language = new HashMap<>();
+        language.put("en", "questions_en.csv");
+
+        Mockito.when(configuration.getQuestionsResources()).thenReturn(language);
         List<Question> questions = questionDao.getAll("en");
 
         assertNotNull(questions);
--- hw-03-student-testing/src/test/java/ru/otus/hw03studenttesting/service/LocalizationServiceImplTest.java
+++ hw-03-student-testing/src/test/java/ru/otus/hw03studenttesting/service/LocalizationServiceImplTest.java
@@ -3,21 +3,27 @@ package ru.otus.hw03studenttesting.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.otus.hw03studenttesting.config.AppConfiguration;
+import ru.otus.hw03studenttesting.config.TestConfig;
 import ru.otus.hw03studenttesting.service.impl.LocalizationServiceImpl;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(classes = {LocalizationServiceImpl.class, AppConfiguration.class})
+@SpringBootTest(classes = {LocalizationServiceImpl.class, AppConfiguration.class, TestConfig.class})
 @DisplayName(value = "Класс LocalizationService")
 public class LocalizationServiceImplTest {
 
     @Autowired
     LocalizationServiceImpl localizationService;
+
+    @MockBean
+    AppConfiguration configuration;
 
     @BeforeEach
     void init() {
@@ -27,6 +33,7 @@ public class LocalizationServiceImplTest {
     @Test
     @DisplayName(value = "Получение всех языков")
     public void getLanguages() {
+        Mockito.when(configuration.getLanguages()).thenReturn(List.of("ру", "en"));
         List<String> languages = localizationService.getLanguages();
 
         assertNotNull(languages);
