c0f00cf7cec988345a8347b5d361cf1e54daba26
Ivan Malyshev <eldorashka@mail.ru>
2022-11-24 18:39:09 +0300

    edit health

--- hw-16-health/src/main/java/ru/otus/ivan/health/HealthCheck.java
+++ hw-16-health/src/main/java/ru/otus/ivan/health/HealthCheck.java
@@ -1,6 +1,7 @@
 package ru.otus.ivan.health;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
@@ -9,19 +10,22 @@ import ru.otus.ivan.repo.BookRepo;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class HealthCheck implements HealthIndicator {
 
     private final BookRepo bookRepo;
 
     @Override
     public Health health() {
-        if (bookRepo.findAll().isEmpty()) {
+        try {
+            bookRepo.findAll();
+            return Health.up().build();
+        } catch (Exception e) {
+            log.error("Сервер не отвечает: " + e.getMessage());
             return Health.down()
                     .status(Status.DOWN)
-                    .withDetail("message", "Что-то пошло не так! Книги не найдены!")
+                    .withDetail("message", "Что-то пошло не так! Сервер не отвечает!")
                     .build();
-        } else {
-            return Health.up().build();
         }
     }
 }
